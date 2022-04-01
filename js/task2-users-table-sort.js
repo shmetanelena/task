03b23744ffd708cea9ca52aae6438489fe835dcf
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-04-01 18:08:53 +0300

    add task

---
+++ js/task2-users-table-sort.js
@@ -0,0 +1,11 @@
+const fields = [
+    {id: 'name', title: 'Name'}, 
+    {id: 'email', title: 'Email'},
+    {id: 'eyeColor', title: 'Eyes'},
+    {id: 'isActive', title: 'Active'},
+    {id: 'gender', title: 'Gender'}
+];
+
+let sortId;
+let isAscendingSort = true;
+
