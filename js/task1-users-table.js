560cc0987b7495ba718b70f11eef88bf3d9937b7
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-04-02 11:25:52 +0300

    training add elements

--- js/task1-users-table.js
+++ js/task1-users-table.js
@@ -1,8 +1,9 @@
 import users from './users.js';
 
-const makeUserTableRowMarkup = ({ name, email, balance }) => {
+const makeUserTableRowMarkup = ({ name, email, balance }, index) => {
     return `
-    <tr>
+    <tr id = "user-${index}">
+        <td>${index}</td>
         <td>${name}</td>
         <td>${email}</td>
         <td>${balance}</td>
@@ -18,3 +19,13 @@ const makeUsersTableRows = users.map(makeUserTableRowMarkup).join('');
 tableEl.insertAdjacentHTML('beforeend', makeUsersTableRows);
 
 console.log(makeUsersTableRows);
+
+//let trEl = document.createElement('tr');
+// let tdEl = document.createElement('td');
+/*<select>
+    <option value="value1">Text1</option>
+    <option value="value2">Text2</option>
+    <option value="value3">Text3</option>
+</select>;
+*/
+//element<select>.value = value1...3
